d61791756b30ef40bb09a16ceb8ed4f9da462a03
Ядерный реактор <noobiko262@gmail.com>
2023-11-28 22:34:58 +0300

    Sqlalchemy orm

--- spambot/core/models/seller.py
+++ spambot/core/models/seller.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.orm import declarative_base, relationship, Mapped
 from datetime import datetime
 
 Base = declarative_base()
@@ -13,9 +13,13 @@ class WholesaleCustomer(Base):
     location = Column(String, default=None)
     associative_words = Column(String, default="запчасти")
     date_add = Column(DateTime, default=datetime.now)
+    customer_type = Column(String)
 
-    social_network = relationship(
-        "SocialNetwork", backref="customer", cascade="all", lazy="select"
+    social_network: Mapped["SocialNetwork"] = relationship(
+        "SocialNetwork",
+        backref="customer",
+        cascade="all, delete-orphan",
+        lazy="joined"
     )
 
 
@@ -23,9 +27,9 @@ class SocialNetwork(Base):
     __tablename__ = "social_network"
 
     customer_id = Column(
-        Integer, ForeignKey("customer.id"), primary_key=True, nullable=False
+        Integer, ForeignKey("customer.id"),
+        primary_key=True, nullable=False
     )
-    type_order = Column(String)
     telegram_url = Column(String, default=None)
     vk_url = Column(String, default=None)
     email = Column(String, default=None)

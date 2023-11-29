3c2dcacb0e6e71bdf1f80f5b454e74447816c7e4
Ядерный реактор <noobiko262@gmail.com>
2023-11-29 20:19:18 +0300

    запуск контейнера

--- spambot/Sending_bots/whatsapp_sending/docker_run_api.py
+++ spambot/Sending_bots/whatsapp_sending/docker_run_api.py
@@ -1,6 +1,7 @@
 import docker
 import keyboard
 import threading
+from docker.errors import NotFound
 
 
 def read_container_logs(container):
@@ -11,47 +12,57 @@ def read_container_logs(container):
         print(f"Ошибка при чтении логов контейнера: {e}")
 
 
-# Подключение к Docker API
-client = docker.from_env()
+def is_container_running(image_name):
+    try:
+        client = docker.from_env()
+        container = client.containers.get(image_name)
+        return container.status == 'running'
+    except NotFound:
+        return False
 
-# Название образа
-image_name = 'devlikeapro/whatsapp-http-api:latest'
 
-# Получение списка локальных образов
-local_images = client.images.list()
+def start_docker_container():
+    # Подключение к Docker API
+    client = docker.from_env()
 
-# Проверка, существует ли образ локально
-image_exists = any(image.tags and image.tags[0] == image_name for image in local_images)
+    # Название образа
+    image_name = 'devlikeapro/whatsapp-http-api:latest'
 
-# Если образа нет локально, скачиваем его
-if not image_exists:
-    print(f"Образ {image_name} не найден локально. Загрузка образа...")
-    client.images.pull(image_name)
-    print("Образ загружен успешно.")
+    # Получение списка локальных образов
+    local_images = client.images.list()
 
-container_params = {
-    'name': 'whatsapp-http-api',  # имя контейнера
-    'remove': True,  # удаление контейнера после завершения
-    'ports': {'3000/tcp': 3000},  # проброс порта 3000
-    'detach': True,  # запуск в фоновом режиме
-}
+    # Проверка, существует ли образ локально
+    image_exists = any(image.tags and image.tags[0] == image_name for image in local_images)
 
-# Запуск контейнера с параметрами
-container = client.containers.run(image_name, **container_params)
+    # Если образа нет локально, скачиваем его
+    if not image_exists:
+        print(f"Образ {image_name} не найден локально. Загрузка образа...")
+        client.images.pull(image_name)
+        print("Образ загружен успешно.")
 
-# Запуск потока для чтения вывода контейнера
-log_thread = threading.Thread(target=read_container_logs, args=(container,), daemon=True)
-log_thread.start()
+    container_params = {
+        'name': 'whatsapp-http-api',  # имя контейнера
+        'remove': True,  # удаление контейнера после завершения
+        'ports': {'3000/tcp': 3000},  # проброс порта 3000
+        'detach': True,  # запуск в фоновом режиме
+    }
 
-# Ожидание ввода пользователя
-print("Для выключения контейнера нажмите клавишу 'Ctrl + Q'.")
-keyboard.wait("Ctrl + q")
+    # Запуск контейнера с параметрами
+    container = client.containers.run(image_name, **container_params)
 
-# Остановка контейнера
-print("Остановка контейнера...")
-container.stop()
+    # Запуск потока для чтения вывода контейнера
+    log_thread = threading.Thread(target=read_container_logs, args=(container,), daemon=True)
+    log_thread.start()
 
-# Ожидание завершения потока
-log_thread.join(timeout=5)  # Подождать завершения потока в течение 5 секунд
+    # Ожидание ввода пользователя
+    print("Для выключения контейнера нажмите клавишу 'Ctrl + Q'.")
+    keyboard.wait("Ctrl + q")
 
-print("Контейнер остановлен.")
+    # Остановка контейнера
+    print("Остановка контейнера...")
+    container.stop()
+
+    # Ожидание завершения потока
+    log_thread.join(timeout=5)  # Подождать завершения потока в течение 5 секунд
+
+    print("Контейнер остановлен.")
